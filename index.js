4565b128a9e7b8a75d17c40c361af366f8f697fb
iGEORGE <www.igeorge.www@gmail.com>
2022-08-26 00:30:07 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+var upload = multer({
+  dest: 'uploads/'
+})
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +15,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file
 
-
+  res.json({
+    name,
+    type,
+    size
+  })
+})
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+
